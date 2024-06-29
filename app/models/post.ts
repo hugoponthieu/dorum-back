0b46c30bd5f913e4fa0249559844fe9e7eca9723
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-06-29 23:01:15 +0200

    migration

--- app/models/post.ts
+++ app/models/post.ts
@@ -1,19 +1,38 @@
+
+
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeSave, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Topic from './topic.js'
 
 export default class Post extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
   @column()
-  declare title: string
+  declare author: string
+  
+  @column()
+  declare content: string
 
   @column()
-  declare postcount: number
+  declare topicId: number
+
+  @belongsTo(() => Topic)
+  declare post: BelongsTo<typeof Topic>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @beforeSave()
+  public static async updatePostCount(post: Post) {
+    const topic = await Topic.find(post.topicId)
+    if (topic) {
+      topic.postcount += 1
+      await topic.save()
+    }
+  }
 }
--- app/models/topic.ts
+++ app/models/topic.ts
@@ -1,19 +1,24 @@
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import Post from './post.js'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
 
 export default class Topic extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
   @column()
-  declare author: string
-  
+  declare title: string
+
   @column()
-  declare content: string
+  declare postcount: number
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @hasMany(() => Post)
+  public posts!: HasMany<typeof Post>
 }
